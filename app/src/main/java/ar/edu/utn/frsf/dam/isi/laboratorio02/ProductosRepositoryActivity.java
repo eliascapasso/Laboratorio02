3c4d592a259f4b97489489c407c312fc20ecc4e8
eliascapasso <elias_capasso@live.com>
2018-09-18 17:14:02 -0300

    inciso 2d andando

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ProductosRepositoryActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ProductosRepositoryActivity.java
@@ -2,6 +2,8 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +24,9 @@ public class ProductosRepositoryActivity extends AppCompatActivity {
     private EditText edtProdCantidad;
     private Button btnProdAddPedido;
     private ProductoRepository productoDAO;
+    private ArrayAdapter<Categoria> adaptadorSpinnerCategorias;
+    private ArrayAdapter<Producto> adaptadorListaProductos;
+    private  Categoria catSeleccionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +40,26 @@ public class ProductosRepositoryActivity extends AppCompatActivity {
 
         productoDAO = new ProductoRepository();
 
-        ArrayAdapter<Categoria> adaptadorSpinnerCategorias = new ArrayAdapter<Categoria>(this, android.R.layout.simple_spinner_item, productoDAO.getCategorias());
+        adaptadorSpinnerCategorias = new ArrayAdapter<Categoria>(this, android.R.layout.simple_spinner_item, productoDAO.getCategorias());
         cmbProductosCategoria.setAdapter(adaptadorSpinnerCategorias);
 
-        //ArrayAdapter<Producto> adaptadorListaProductos = new ArrayAdapter<Producto>(this, android.R.layout.simple_list_item_1, cmbProductosCategoria.setOnItemClickListener(););
-        //lstProductos.setAdapter(adaptadorListaProductos);
-        //TODO: llegamos hasta acá
+        catSeleccionada = productoDAO.getCategorias().get(1);
+        cmbProductosCategoria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id)
+            {
+                catSeleccionada = adaptadorSpinnerCategorias.getItem(position);
+
+                adaptadorListaProductos = new ArrayAdapter<Producto>(ProductosRepositoryActivity.this, android.R.layout.simple_list_item_1, productoDAO.buscarPorCategoria(catSeleccionada));
+                lstProductos.setAdapter(adaptadorListaProductos);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView)
+            {
+                //Nada seleccionado
+            }
+        });
     }
 }
