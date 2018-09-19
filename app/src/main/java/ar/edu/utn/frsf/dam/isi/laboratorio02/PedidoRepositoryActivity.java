349f06b372b0425e5522fc0c36b4b85d4488aefd
eliascapasso <elias_capasso@live.com>
2018-09-19 14:42:21 -0300

    ejercicio 3d andando

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoRepositoryActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoRepositoryActivity.java
@@ -2,10 +2,12 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
@@ -14,6 +16,7 @@ import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
 public class PedidoRepositoryActivity extends AppCompatActivity {
 
     private EditText edtMail;
+    private RadioGroup rgOptEntrega;
     private RadioButton optRetira;
     private RadioButton optEnviar;
     private EditText edtDireccion;
@@ -33,22 +36,31 @@ public class PedidoRepositoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido_repository);
 
-        edtMail = (EditText) findViewById(R.id.edtPedidoMail);
+        edtMail = (EditText) findViewById(R.id.editPedidoCorreo);
+        rgOptEntrega = (RadioGroup)findViewById(R.id.optPedidoModoEntrega);
         optRetira = (RadioButton) findViewById(R.id.optPedidoRetira);
         optEnviar = (RadioButton) findViewById(R.id.optPedidoEnviar);
         edtDireccion = (EditText) findViewById(R.id.edtPedidoDireccion);
         edtHoraSolicitada = (EditText) findViewById(R.id.edtPedidoHoraEntrega);
-        lstPedidos = (ListView) findViewById(R.id.lstPedidos);
+        lstPedidos = (ListView) findViewById(R.id.lstPedidoItems);
         btnAddProducto = (Button) findViewById(R.id.btnPedidoAddProducto);
         btnQuitarProducto = (Button) findViewById(R.id.btnPedidoQuitarProducto);
         btnHacerPedido = (Button) findViewById(R.id.btnPedidoHacerPedido);
         btnVolver = (Button) findViewById(R.id.btnPedidoVolver);
 
-        if(optEnviar.isChecked()){
-            edtDireccion.setEnabled(false);
-        }
-        else{
-            edtDireccion.setEnabled(true);
-        }
+        edtDireccion.setEnabled(false);
+        rgOptEntrega.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(optEnviar.isChecked()){
+                    edtDireccion.setEnabled(true);
+                }
+                else{
+                    edtDireccion.setEnabled(false);
+                }
+            }
+        });
+
     }
 }
