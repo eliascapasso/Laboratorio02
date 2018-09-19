e58130b8e7e9a1ae5d652d90e486ad459b00b544
eliascapasso <elias_capasso@live.com>
2018-09-19 14:21:59 -0300

    3d en proceso

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoRepositoryActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoRepositoryActivity.java
@@ -2,12 +2,53 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
 
 public class PedidoRepositoryActivity extends AppCompatActivity {
+
+    private EditText edtMail;
+    private RadioButton optRetira;
+    private RadioButton optEnviar;
+    private EditText edtDireccion;
+    private EditText edtHoraSolicitada;
+    private ListView lstPedidos;
+    private Button btnAddProducto;
+    private Button btnQuitarProducto;
+    private Button btnHacerPedido;
+    private Button btnVolver;
+
+    private Pedido unPedido;
+    private PedidoRepository repositorioPedido;
+    private ProductoRepository repositorioProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido_repository);
+
+        edtMail = (EditText) findViewById(R.id.edtPedidoMail);
+        optRetira = (RadioButton) findViewById(R.id.optPedidoRetira);
+        optEnviar = (RadioButton) findViewById(R.id.optPedidoEnviar);
+        edtDireccion = (EditText) findViewById(R.id.edtPedidoDireccion);
+        edtHoraSolicitada = (EditText) findViewById(R.id.edtPedidoHoraEntrega);
+        lstPedidos = (ListView) findViewById(R.id.lstPedidos);
+        btnAddProducto = (Button) findViewById(R.id.btnPedidoAddProducto);
+        btnQuitarProducto = (Button) findViewById(R.id.btnPedidoQuitarProducto);
+        btnHacerPedido = (Button) findViewById(R.id.btnPedidoHacerPedido);
+        btnVolver = (Button) findViewById(R.id.btnPedidoVolver);
+
+        if(optEnviar.isChecked()){
+            edtDireccion.setEnabled(false);
+        }
+        else{
+            edtDireccion.setEnabled(true);
+        }
     }
 }
