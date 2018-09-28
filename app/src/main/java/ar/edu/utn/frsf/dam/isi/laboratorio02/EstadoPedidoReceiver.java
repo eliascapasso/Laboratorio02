22cfdad17d76b257fdff8f59a2e25f8d089e8aa8
eliascapasso <elias_capasso@live.com>
2018-09-27 21:50:54 -0300

    Ejercicio 1 en proceso (no anda)

---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/EstadoPedidoReceiver.java
@@ -0,0 +1,32 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
+
+public class EstadoPedidoReceiver extends BroadcastReceiver {
+
+    public static String ESTADO_ACEPTADO = "ACEPTADO";
+
+    private PedidoRepository pedidoRepository = new PedidoRepository();
+    private Pedido unPedido;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        Bundle extras = intent.getExtras();
+        int idPedido = extras.getInt("idPedido");
+
+        unPedido = pedidoRepository.buscarPorId(idPedido);
+
+        Toast.makeText(context, "Info Pedido", Toast.LENGTH_LONG).show();
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
